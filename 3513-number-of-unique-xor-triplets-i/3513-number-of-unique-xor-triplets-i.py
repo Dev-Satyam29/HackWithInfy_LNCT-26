98db0000f4a835fc83e13055bf73c0896d21918b
Dev-Satyam29 <gupta29satyam@gmail.com>
2026-07-23 12:16:31 +0530

    Time: 3 ms (20.34%), Space: 32 MB (61.02%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
         n=len(nums)
         if n<=2:
@@ -6,6 +6,18 @@ class Solution:
         mask=0
         for i in nums:
             mask |=i
-        return mask+1
+        return mask+1'''
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+            n = len(nums)
+            m = n
+            
+            m |= m >> 1
+            m |= m >> 2
+            m |= m >> 4
+            m |= m >> 8
+            m |= m >> 16
+            
+            return (m + 1) >> (3 // (n + 1))
 
-        
+            
